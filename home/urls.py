c4fb6e36c8a46dd80b81d490c13a5a760bffc439
harshitbansall <harshitbansal587@gmail.com>
2024-08-07 23:29:28 +0530

    Updates

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import Dashboard
+from .views import Dashboard, DashboardAccountValueGraph
 
 urlpatterns = [
     path('dashboard', Dashboard.as_view(), name='Dashboard'),
+    path('accountValueGraph', DashboardAccountValueGraph.as_view(), name='DashboardAccountValueGraph'),
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 import json
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.contrib.auth import authenticate, login
 from django.shortcuts import HttpResponse, redirect, render
@@ -13,7 +13,6 @@ from .serializers import PaymentSerializer
 
 class Dashboard(APIView):
     def get(self, request):
-        # time.sleep(2)
         payments_qs = request.user.payment_set.all()
         amount_invested = round(sum([x.amount for x in payments_qs]), 2)
 
@@ -38,5 +37,50 @@ class Dashboard(APIView):
             "total_portfolio": {"float": (amount_invested + earned_interest), "string": f"₹ {(amount_invested + earned_interest):,.2f}"},
             "daily_earnings": {"float": daily_earnings, "string": f"₹ {daily_earnings:,.2f}"},
             "recent_fundings": PaymentSerializer(payments_qs[:5], many=True).data,
+        }
+        return Response(data=data, status=status.HTTP_200_OK)
+
+
+class DashboardAccountValueGraph(APIView):
+    def get(self, request):
+        date_today = datetime.today().replace(
+            tzinfo=None, hour=0, minute=0, second=0, microsecond=0)
+        user_date_joined = request.user.date_joined.replace(
+            tzinfo=None, hour=0, minute=0, second=0, microsecond=0)
+
+        days_from_joined = (date_today - user_date_joined).days
+
+        payments_qs = request.user.payment_set.all()
+
+        if payments_qs.exists() == False:
+            return Response(data={"success": "true","data": []}, status=status.HTTP_200_OK)
+
+        all_payments = {}
+        for payment in payments_qs:
+            all_payments[payment.created_at.replace(tzinfo=None, hour=0, minute=0, second=0, microsecond=0)] = [j.amount for j in payments_qs if j.created_at.replace(
+                tzinfo=None, hour=0, minute=0, second=0, microsecond=0) == payment.created_at.replace(tzinfo=None, hour=0, minute=0, second=0, microsecond=0)]
+
+        return_data = []
+        currentInvested = 0
+        cuurentInterest = 0
+        for x in range(days_from_joined):
+            currentDate = (user_date_joined + timedelta(days=x))
+
+            currentInvested += (currentInvested * 0.00041)
+
+            if currentDate in all_payments.keys():
+                currentInvested += sum(all_payments[currentDate])
+
+            return_data += [
+                {
+                    "date": currentDate.strftime('%Y-%m-%d'),
+                    "invested": round(currentInvested, 2),
+                    "interest": round((currentInvested * 0.00041) * x,2)
+                }
+            ]
+
+        data = {
+            "success": "true",
+            "data": return_data
         }
         return Response(data=data, status=status.HTTP_200_OK)
